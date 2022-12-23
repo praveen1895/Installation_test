47038e0f6ac5cfdb30da7fa98a40d6ff4315831d
shivaprakashbc <114686297+shivaprakashbc@users.noreply.github.com>
2022-12-23 23:55:48 +0530

    Update test.sh

--- installation/test.sh
+++ installation/test.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-#install_aws_cli()
-#{
+install_aws_cli()
+{
     sudo apt-get update
     sudo apt-get --assume-yes install awscli 
     if [ $? -ne 0 ]; then
        echo "error while installing awscli"
     fi
     echo "awscli installed successfully"
-#}
-#install_netcat()
-#{
+}
+install_netcat()
+{
     sudo apt-get update
     sudo apt-get -y install netcat-traditional
     if [ $? -ne 0 ]; then
        echo "error while installing netcat"
     fi
     echo "netcat installed successfully"
-#}
+}
 
-#install_aws_cli
+install_aws_cli
 aws --version
-#install_netcat
+install_netcat
 #netcat --version
 nc -h
